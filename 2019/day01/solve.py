e3bac6f9c69d04e578244069a16468cf05d53c49
Christof Haerens <christof@haerens.be>
2019-12-02 14:55:05 +0100

    add 2019 - day1+2

---
+++ 2019/day01/solve.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import re
+from collections import defaultdict
+
+day = "--- Day 1 - 2019 ---"
+
+
+def calculate_fuel(mass, all):
+    total_mass = 0
+    calculate = True
+    while calculate:
+        mass = int(mass / 3) - 2
+        if all is False:
+            return mass
+        if mass > 0:
+            total_mass += mass
+        else:
+            calculate = False
+    return total_mass
+
+
+def solve1(data):
+    return sum([calculate_fuel(d, False) for d in data])
+
+
+def solve2(data):
+    return sum([calculate_fuel(d, True) for d in data])
+
+
+def solve(data):
+    a1 = solve1(data)
+    a2 = solve2(data)
+    print("\n%s" % day)
+    print("part1 = %r" % a1)
+    print("part2 = %r" % a2)
+    print()
+
+
+def main():
+    fh = open('./input.txt', 'r')
+    data = [int(line.strip()) for line in fh]
+    fh.close()
+    solve(data)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2019/day02/solve.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+
+import re
+from collections import defaultdict
+
+day = "--- Day 2 - 2019 ---"
+
+
+def intcode(data, noun, verb):
+    pos = 0
+    data[1] = noun
+    data[2] = verb
+    run = True
+    while run:
+        opcode = data[pos]
+        # print(data[pos:pos + 4])
+        if opcode == 99:
+            run = False
+        else:
+            a1, a2, a3 = data[pos + 1:pos + 4]
+            pos += 4
+            if opcode == 1:
+                data[a3] = data[a1] + data[a2]
+            elif opcode == 2:
+                data[a3] = data[a1] * data[a2]
+    return data[0]
+
+
+def solve1(data):
+    return intcode(data.copy(), 12, 2)
+
+
+def solve2(data):
+    r2 = 19690720
+    for noun in range(100):
+        for verb in range(100):
+            if r2 == intcode(data.copy(), noun, verb):
+                return noun * 100 + verb
+
+
+def solve(data):
+    print("\n%s" % day)
+    print("part1 = %r" % solve1(data))
+    print("part2 = %r" % solve2(data))
+    print()
+
+
+def main():
+    fh = open('./input.txt', 'r')
+    # fh = open('./input_test.txt', 'r')
+    input = [line.strip() for line in fh]
+    fh.close()
+    data = [int(d) for d in input[0].split(',')]
+    solve(data)
+
+
+if __name__ == '__main__':
+    main()
